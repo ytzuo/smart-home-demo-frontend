18a701305db73163b6447e3411cd5e417c9e0b56
Xiao_Chen <18197067230@163.com>
2025-08-28 16:04:41 +0800

    增加“具体”界面

---
+++ app/src/main/java/com/SmartHome/SmartHomeDemo/SpecificActivity.java
@@ -0,0 +1,16 @@
+package com.SmartHome.SmartHomeDemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpecificActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specific);
+
+
+    }
+
+}
